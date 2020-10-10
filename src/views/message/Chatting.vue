f19b5f2fced9a829625bb3c90ec26d0e0c54f634
Weng Hao Min <7371599@qq.com>
2020-10-10 10:48:26 +0800

    消息2

--- src/views/message/Chatting.vue
+++ src/views/message/Chatting.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="chatting">
-
     <message-nearby></message-nearby>
 
     <div v-for="(item, index) in chatMan" :key="index" class="all">
@@ -9,6 +8,7 @@
         <p>{{ item.name }}</p>
         <span>{{ item.msg }}</span>
       </div>
+      <span class="time">{{ item.time }}</span>
     </div>
   </div>
 </template>
@@ -25,48 +25,56 @@ export default {
           msg: "好友喊你来撸猫啦",
           img:
             "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=348959600,4149101165&fm=26&gp=0.jpg",
+          time: "刚刚",
         },
         {
           name: "通知",
           msg: "好友喊你来撸狗啦",
           img:
             "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3036122436,2666208738&fm=26&gp=0.jpg",
+          time: "昨天",
         },
         {
           name: "金凯瑞",
           msg: "好友喊你来撸猪啦",
           img:
             "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2582345274,3041754039&fm=26&gp=0.jpg",
+          time: "05-20",
         },
         {
           name: "小总管",
           msg: "好友喊你来撸牛啦",
           img:
             "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1086772439,2240584529&fm=26&gp=0.jpg",
+          time: "05-20",
         },
         {
           name: "人人",
           msg: "好友喊你来撸羊啦",
           img:
             "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1150861197,1123781499&fm=26&gp=0.jpg",
+          time: "05-20",
         },
         {
           name: "万千世界",
           msg: "好友喊你来撸龙啦",
           img:
             "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1619711063,2414271044&fm=26&gp=0.jpg",
+          time: "05-19",
         },
         {
           name: "兔爷",
           msg: "好友喊你来撸鲲啦",
           img:
             "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2509371347,2043609233&fm=26&gp=0.jpg",
+          time: "05-19",
         },
         {
           name: "印度小黑猪",
           msg: "好友喊你来撸屎啦",
           img:
             "https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2106168639,981360299&fm=26&gp=0.jpg",
+          time: "05-18",
         },
       ],
     };
@@ -101,6 +109,11 @@ export default {
       span {
         font-size: 12px;
       }
+    }
+    .time {
+      font-size: 12px;
+      width: 50px;
+      color: #999;
     }
   }
 }
